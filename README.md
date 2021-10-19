0dd2dc258e902fb6f8120cf2b8fc6670731eacce
Teymur Gahramanov <teymur_gahramanov@cbar.az>
2021-10-19 17:31:00 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# DockPost
+#TODO
+TLS
++ no-reply
+
+docker run --rm -d -p 25:25 -e MAIL_DOMAIN='yourDomain.com' -e MAIL_HOST='mail.yourDomain.com' -e SMTP_USER='yourUser:yourPass' dockercomposemailserver
---
+++ build/entrypoint.sh
@@ -0,0 +1,70 @@
+#Postfix
+postconf -e myhostname=$MAIL_HOST
+postconf -e mydestination=localhost,localhost.localdomain,localhost
+postconf -e maillog_file=/dev/stdout
+postconf -e inet_protocols=ipv4
+postconf -e smtpd_sasl_auth_enable=yes
+postconf -e broken_sasl_auth_clients=yes
+postconf -e smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination
+postconf -e strict_rfc821_envelopes=yes
+postconf -e show_user_unknown_table_name=no
+postconf -e smtpd_helo_required=yes
+postconf -e smtp_always_send_ehlo=yes
+postconf -e smtpd_hard_error_limit=3
+postconf -e smtpd_error_sleep_time=10s
+postconf -e smtpd_timeout=60s
+postconf -e smtp_helo_timeout=30s
+postconf -e smtp_mail_timeout=30s
+postconf -e smtp_rcpt_timeout=30s
+postconf -e unverified_sender_reject_code=550
+postconf -e unknown_local_recipient_reject_code=550
+postconf -e smtp_header_checks=pcre:/etc/postfix/smtp_header_checks
+postconf -e virtual_alias_maps=hash:/etc/postfix/virtual
+cat >> /etc/postfix/smtp_header_checks << EOF
+/^Received: .*/     IGNORE
+/^X-Originating-IP:/    IGNORE
+/^X-Mailer:/            IGNORE
+/^Mime-Version:/        IGNORE
+EOF
+echo 'null: /dev/null' > /etc/aliases
+echo "no-reply@$MAIL_DOMAIN null" > /etc/postfix/virtual
+newaliases
+postmap /etc/postfix/virtual
+postconf -F '*/*/chroot = n'
+
+#TLS (NOT TESTED)
+if [ $TLS_ENABLE = 'YES' ]; then
+  mkdir /etc/postfix/tls && chown postfix /etc/postfix/tls && chmod 400 /etc/postfix/tls
+  postconf -e smtpd_use_tls=yes
+  postconf -e smtpd_tls_key_file=/etc/postfix/tls/cert.pem
+  postconf -e smtpd_tls_cert_file=/etc/postfix/tls/key.pem
+  postconf -e smtpd_tls_CAfile=/etc/postfix/tls/ca.pem
+  postconf -e smtpd_tls_security_level=encrypt
+  postconf -e smtpd_tls_loglevel=0
+  postconf -e smtpd_tls_received_header=yes
+  postconf -e smtpd_tls_session_cache_timeout=3600s
+  postconf -e smtp_use_tls=yes
+  postconf -e smtp_tls_key_file=/etc/postfix/tls/cert.pem
+  postconf -e smtp_tls_cert_file=/etc/postfix/tls/key.pem
+  postconf -e smtp_tls_CAfile=/etc/postfix/tls/ca.pem
+  postconf -e smtp_tls_security_level=encrypt
+  postconf -e smtp_tls_loglevel=0
+  postconf -e smtp_tls_session_cache_timeout=3600s
+  postconf -e tls_random_source=dev:/dev/urandom
+fi
+
+#SASL
+cat >> /etc/postfix/sasl/smtpd.conf <<EOF
+pwcheck_method: auxprop
+auxprop_plugin: sasldb
+mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM
+EOF
+echo $SMTP_USER | tr , \\n > /tmp/passwd
+while IFS=':' read -r _user _pass; do
+  echo $_pass | saslpasswd2 -p -c -u $MAIL_DOMAIN $_user
+done < /tmp/passwd
+chown postfix:sasl /etc/sasldb2
+
+###
+rm -f /tmp/passwd
+postfix start-fg
